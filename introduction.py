4271d0f6bd7773596f54b4d0f92e8a95544d02a3
iXpertsudo <kamauvictor471@gmail.com>
2026-02-04 08:19:18 +0300

    First Python commit

---
+++ introduction.py
@@ -0,0 +1,11 @@
+
+#This is  a  comment
+#A comment explains what the code does. Notes you leave for yourself or others.
+#Print is  a method used to display an output.
+
+
+
+print ("Welcome to Python Programming")
+print("Victor")
+print(100)
+print(20 * 3 )
---
+++ variables.py
@@ -0,0 +1,28 @@
+# Variable - A container that is used to store  a value
+
+school = "eMobilis"
+firstname = "Victor"
+lastname = "Smith"
+num1 = 36
+num2 = 42
+language = "Python"
+
+
+print(firstname)
+print(lastname)
+print(school)
+print(language)
+print(num1)
+
+
+
+print(school)
+print(firstname, lastname)
+print(num1 + num2)
+
+
+
+print(language, "is a web programming language")
+
+
+print("The sum of" ,num1 ,"and" , num2, "is", num1 + num2)
